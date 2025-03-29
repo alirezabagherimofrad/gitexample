d17b7231f58e328d954cc4efb2d807f94a708e0d
alirezabagherimofrad <alirezabagherimofrad>
2025-03-29 13:38:23 +0330

    console.writline2

--- gitexample/Program.cs
+++ gitexample/Program.cs
@@ -7,6 +7,7 @@
             
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Alireza abgheri");
+            Console.WriteLine("Agha Momeni");
         }
 
         [hassan]
