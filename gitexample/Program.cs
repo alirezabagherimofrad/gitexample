b1811d845fe80c0e2038401bc4c75c62e153e7f3
alirezabagherimofrad <alirezabagherimofrad>
2025-03-29 13:36:00 +0330

    console.writeline

--- gitexample/Program.cs
+++ gitexample/Program.cs
@@ -4,8 +4,15 @@
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello, World!");
-            Console.WriteLine();
+            Console.WriteLine("Alireza abgheri");
+        }
+
+        [hassan]
+        public string gggg(string name)
+        {
+            return name;
         }
 
     }
---
+++ gitexample/hassan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitexample
+{
+    public class hassan:Attribute
+    {
+        public void nnnn()
+        {
+            Console.WriteLine("ggggdikjfvnmsdfxikuvbns");
+        }
+    }
+}
